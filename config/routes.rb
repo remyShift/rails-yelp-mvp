3f84537d1e13ce40150426f04df3adef7907b4b3
Remy Cassagne <remy.engassac@icloud.com>
2024-11-13 16:34:50 +0100

    'done'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "restaurants#index"
   resources :restaurants do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :new]
   end
 end
